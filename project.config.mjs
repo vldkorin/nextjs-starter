e30c31c67862ee98819e3b4ffe58264c347f7ea8
Vladyslav Korin <vladyslav.korin@binary-studio.com>
2026-04-23 21:15:27 +0300

    chore(root): add commit conventions config IP-1

---
+++ project.config.mjs
@@ -0,0 +1,21 @@
+export const ProjectPrefix = {
+  CHANGE_TYPES: [
+    "build",
+    "chore",
+    "ci",
+    "docs",
+    "feat",
+    "fix",
+    "perf",
+    "refactor",
+    "revert",
+    "style",
+    "test"
+  ],
+  ISSUE_PREFIXES: ["IP", "RELEASE"],
+  SCOPE: {
+    COMMON: ["root", "config", "infra", "ci", "release"],
+    APP: ["web"],
+    CODE: ["app", "components", "lib", "api", "db"]
+  }
+};
